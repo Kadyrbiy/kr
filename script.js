681cab1b530983fde6043eb80b2f52ca8e9def43
kadyrbiy <flavohomo@gmail.com>
2022-01-30 13:17:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// let cinemaPrice = {
+//    BishkekPark: 250,
+//    DordoiPlaza: 300,
+//    Vefa: 200,
+//    AlaToo: 150,
+//    Manas: 100,
+//    AsiaMall: 250,
+// }
+
+// let option;
+// for (option in cinemaPrice) {
+//    document.write(option + ':' + cinemaPrice[option] + '<br/>')
+   
+// }
+
+// let cars = {
+//    Mazda: 250,
+//    Carvate: 300,
+//    Tayota: 200,
+//    Honda: 150,
+//    Tesla: 100,
+// }
+
+// let option;
+// for (option in cars) {
+//    document.write(option + ':' + cars[option] + '<br/>')
+// }
+
+// ! Методы
+
+document.getElementsByClassName('block')[0].style.color = 'red';
+
+
+// !
+let city = document.createElement('city')
+city.className = 'city' ;
+city.innerHTML = '<h1>London<h1/>';
+city.style.color = 'purple'
+
+document.body.append(city)
